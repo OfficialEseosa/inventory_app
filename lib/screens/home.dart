bcb9089ad3dbc98c79ef44da2e636e1b9c44138f
Raphael Omorose <raphaelomorose@gmail.com>
2026-04-06 13:44:44 -0400

    Initial commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Inventory App'),
+      ),
+    );
+  }
+}
